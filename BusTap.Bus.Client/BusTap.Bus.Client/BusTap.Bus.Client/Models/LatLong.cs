c488b82ecdfd26094b21dd5f22f2fdf0ff7cb3ba
mark.mendoza@iqmetrix.com <Mark.Mendoza@iqmetrix.com>
2017-01-18 10:26:55 +0800

    Initial commit

---
+++ BusTap.Bus.Client/BusTap.Bus.Client/BusTap.Bus.Client/Models/LatLong.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTap.Bus.Client.Models
+{
+    public class LatLong
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
